659b47c8c179cdd3ec5dd2daeda982ad254adbea
bradvatne <brad.vatne@gmail.com>
2023-05-20 15:24:57 +0800

    space age

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,3 +1,19 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+interface Planets {
+  [key: string]: number;
+}
+
+const SEC_PER_YEAR = 31557600;
+
+const RATIOS: Planets = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+export function age(planet: string, seconds: number): number {
+  return Number((seconds / SEC_PER_YEAR / RATIOS[planet]).toFixed(2));
 }

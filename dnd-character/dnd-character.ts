baca72ff917d7d4f05547f86e04ee4b9d1123595
brad <brad.vatne@gmail.com>
2023-07-21 17:01:06 +0800

    dnd character

--- dnd-character/dnd-character.ts
+++ dnd-character/dnd-character.ts
@@ -1,9 +1,41 @@
 export class DnDCharacter {
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
+  hitpoints: number;
+  
+  constructor() {
+    this.strength = DnDCharacter.generateAbilityScore()
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution)
+  }
+  
+  private static diceRoll(): number {
+    return Math.floor(Math.random()*6) +1;
+  }
+  
   public static generateAbilityScore(): number {
-    
+    let rolls = []
+    let lowest = 7;
+    for (let i =0; i < 4; i++) {
+      let newVal = DnDCharacter.diceRoll();
+      if (newVal < lowest) lowest = newVal
+      rolls.push(newVal)
+    }
+    rolls.splice(rolls.indexOf(lowest), 1); // remove the lowest roll
+    const res = rolls.reduce((a,b) => a+b);
+    return res;
   }
 
   public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this statement and implement this function')
+     return Math.floor((abilityValue - 10) / 2);
   }
 }
+

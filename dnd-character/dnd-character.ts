ad13eb5629c27f3db0f511dd14893c01b37fa969
brad <brad.vatne@gmail.com>
2023-07-21 16:39:55 +0800

    dnd

--- dnd-character/dnd-character.ts
+++ dnd-character/dnd-character.ts
@@ -1,6 +1,6 @@
 export class DnDCharacter {
   public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
+    
   }
 
   public static getModifierFor(abilityValue: number): number {

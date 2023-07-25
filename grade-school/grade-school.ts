5b40f8d81ba62f57d2a37edf415b0920fa3875d7
brad <brad.vatne@gmail.com>
2023-07-25 11:09:25 +0800

    grade school.. weird deep-copy function

--- grade-school/grade-school.ts
+++ grade-school/grade-school.ts
@@ -1,16 +1,53 @@
-export class GradeSchool {
-  students = new Map<number, string[]>();
+type Roster = {
+  [key: number]: string[];
+};
 
-  roster(): Map<number, string[]> {
-    return this.students;
+const sample = {
+  1: ["Hello"],
+  5: ["Bob", "Jim"],
+};
+
+export class GradeSchool {
+  private school: Roster;
+
+  constructor() {
+    this.school = {};
+  }
+
+  private deepCopy(obj: any): any {
+    if (typeof obj !== "object" || obj === null) {
+      return obj;
+    }
+
+    if (Array.isArray(obj)) {
+      return obj.map((item) => this.deepCopy(item));
+    }
+
+    const newObj: { [key: string]: any } = {};
+    Object.keys(obj).forEach((key) => {
+      newObj[key] = this.deepCopy(obj[key]);
+    });
+
+    return newObj;
+  }
+
+  roster(): Roster {
+    return this.deepCopy(this.school);
   }
 
   add(name: string, grade: number): void {
-    const currentStudents = this.students.get(grade)!;
-    this.students.set(grade, [...currentStudents, name]);
+    for (let gradeKey of Object.keys(this.school)) {
+      this.school[parseInt(gradeKey)] = this.school[parseInt(gradeKey)].filter(
+        (currentName) => currentName !== name
+      );
+    }
+    const currentStudents = this.school[grade];
+    currentStudents
+      ? (this.school[grade] = [...currentStudents, name].sort())
+      : (this.school[grade] = [name]);
   }
 
-  grade(name: string): number {
-    return this.students.has(name)!;
+  grade(grade: number): string[] {
+    return this.school[grade] ? [...this.school[grade]] : [];
   }
 }

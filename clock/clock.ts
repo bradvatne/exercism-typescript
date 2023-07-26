723cbd3526856ab6d4b4082b15a19602fb77fb2d
brad <brad.vatne@gmail.com>
2023-07-26 11:29:16 +0800

    clock

--- clock/clock.ts
+++ clock/clock.ts
@@ -1,21 +1,33 @@
 export class Clock {
-  constructor(hour: unknown, minute?: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  private readonly _hour: number;
+
+  private readonly _minute: number;
+
+  constructor(hour: number, minute?: number) {
+    let m = minute ?? 0;
+
+    this._minute = ((m % 60) + 60) % 60;
+
+    this._hour = (((hour + Math.floor(m / 60)) % 24) + 24) % 24;
   }
 
-  public toString(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public toString(): string {
+    const h = ["0", ...this._hour.toString(10)].slice(-2).join("");
+
+    const m = ["0", ...this._minute.toString(10)].slice(-2).join("");
+
+    return `${h}:${m}`;
   }
 
-  public plus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+  public plus(minutes: number): Clock {
+    return new Clock(this._hour, this._minute + minutes);
   }
 
-  public minus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+  public minus(minutes: number): Clock {
+    return new Clock(this._hour, this._minute - minutes);
   }
 
-  public equals(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public equals(other: Clock): boolean {
+    return other._hour === this._hour && other._minute === this._minute;
   }
 }
